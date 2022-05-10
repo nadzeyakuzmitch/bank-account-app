a32e3aca15053039d734d0ebdc59e014ff5240de
Nadzeya Kuzmitch <nk83@njit.edu>
2022-05-10 15:05:54 -0500

    added context processors for form, year and env variable

---
+++ app/context_processors/__init__.py
@@ -0,0 +1,24 @@
+"""These are reusable template function"""
+import datetime
+from os import getenv
+
+from app.auth.forms import login_form
+
+
+def utility_text_processors():
+    form = login_form()
+
+    def deployment_environment():
+        return getenv('FLASK_ENV', None)
+
+    def current_year():
+        currentDateTime = datetime.datetime.now()
+        date = currentDateTime.date()
+        year = date.strftime("%Y")
+        return year
+
+    return dict(
+        form=form,
+        deployment_environment=deployment_environment(),
+        year=current_year()
+    )
